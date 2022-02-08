8c42219d32fca2f7f29425a49f11ea02f9836a74
David Wampamba <davidwampamba@gmail.com>
2022-02-08 23:51:14 +0300

    Remove redundant card

--- src/HowTo.js
+++ src/HowTo.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Card from 'react-bootstrap/Card'
 
 function HowTo() {
     return <Container>
