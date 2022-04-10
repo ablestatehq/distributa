cb709025665444fcb0c744c204952782e46a5754
David Wampamba <davidwampamba@gmail.com>
2022-04-10 19:59:10 +0300

    Read me

--- README.md
+++ README.md
@@ -1,70 +1,46 @@
-# Getting Started with Create React App
+# Distributa 💰
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Allocate Income to Team members in < 2mins using this Open Source tool.
 
-## Available Scripts
+## How it works
 
-In the project directory, you can run:
+### Share Earnings
+1. Enter the Amount
+2. Add a benecificiary and portion in percentage.
+3. See what everyone gets in real time
+4. What to allocated a different amount? Reset and start afresh.
 
-### `yarn start`
+### Invoice
+You don't need to spend money to send an invoice. Distributa gives you the super powers for free.
+1. Click Invoice
+2. Enter details
+3. Click download to get a PDF invoice for free
+4. Send the invoice using a medium of your preference
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+## Want to contribute?
+Without your hand there is no way this tool can go far. If there is anyway you can contribute let me know through raising an issue.
+You can also give it a star, that way I get to know that this is important and I give it more time.
 
-The page will reload when you make changes.\
-You may also see any lint errors in the console.
+### Contribute with code/writing
+1. Fork
+2. Clone to your computer
+3. Navigate to the project directory and excute yarn add or npm install
+4. Execute yarn start or npm start
+5. Add and test your code
+6. Push the code to your repo
+7. Send me a PR
+8. Sitback and relax, you will get my response once I merge your code or if I have questions.
+9. Check distributa.vercel.app to see how the app works.
 
-### `yarn test`
+## Why contribute
+1. As the initiator of this project I will be motivated to continue working on it and adding more features.
+2. If this is a tech stack and project you're interested in, it gives you the opportunity to add your effort to it.
+3. If you're looking to gain experience and have some working tool on your portfolio why not go for it.
+4. It motivates others to use this tool and join us.
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+## Tech stack used
+-> React.JS
+-> Bootstrap
+-> Vercel.com deployment
 
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can't go back!**
-
-If you aren't satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you're on your own.
-
-You don't have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn't feel obligated to use this feature. However we understand that this tool wouldn't be useful if you couldn't customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `yarn build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Thank you 👍🏽
--- src/Invoice.js
+++ src/Invoice.js
@@ -6,222 +6,319 @@ import { PDFDownloadLink } from "@react-pdf/renderer";
 const initialValues = {
 	sender: "",
 	receiver: "",
-	shipping: "",
+	shipping_address: "",
 	bill_number: "",
 	on_date: "",
 	terms: "",
 	due_date: "",
 	po_number: "",
+	other_terms: "",
+	notes: "",
+	discount:"",
+	tax:"",
+	shipping:"",
+	paid:"",
+	balance_due:"",
 	items: [
 		{
 			title: "",
-			qty: 0,
-			rate: 0,
-			amount: 0,
+			qty: "",
+			rate: "",
+			amount: "",
 		},
 	],
 };
 
-const Invoice = () => (
-	<div>
-		<h2>Generate Invoice</h2>
-		<Formik
-			initialValues={initialValues}
-			onSubmit={async (values) => {
-				await new Promise((r) => setTimeout(r, 500));
-				alert(JSON.stringify(values, null, 2));
-			}}>
-			{({ values }) => (
-				<Form>
-					<Row>
-						<Col md="9">
-							<Row className="mt-4 mb-4">
-								<Col className="d-flex flex-column justify-content-end">
-									Logo
-									<Field
-										as="textarea"
-										placeholder="Sender's information"
-										className="form-control mb-4"
-										name="sender"
-									/>
-									<Field
-										as="textarea"
-										placeholder="Receiver's information"
-										className="form-control"
-										name="receiver"
-									/>
-								</Col>
-								<Col className="d-flex flex-column justify-content-end">
-									<Field
-										as="textarea"
-										placeholder="Shipping details"
-										className="form-control"
-										name="shipping"
-									/>
-								</Col>
-								<Col className="d-flex flex-column justify-content-end">
-									<div className="d-flex flex-row justify-content-between">
-										<label htmlFor="bill_number">Number</label>
+const subTotal = (values) => {
+	return values.items.reduce((acc, curr) => {
+		return (acc + parseInt(curr.amount));
+	}, 0);
+};
+
+const Invoice = () => {
+	return (
+		<div>
+			<h2>Generate Invoice</h2>
+			<Formik
+				initialValues={initialValues}
+				onSubmit={async (values) => {
+					await new Promise((r) => setTimeout(r, 500));
+					alert(JSON.stringify(values, null, 2));
+				}}>
+				{({ values }) => (
+					<Form>
+						<Row>
+							<Col md="9">
+								<Row className="mt-4 mb-4">
+									<Col className="d-flex flex-column justify-content-end">
+										Logo
 										<Field
-											type="text"
-											name="bill_number"
-											className="p-0 form-control"
+											as="textarea"
+											placeholder="Sender's information"
+											className="form-control mb-4"
+											name="sender"
 										/>
-									</div>
-									<div className="d-flex flex-row justify-content-between">
-
-									<label htmlFor="date">Date</label>
-									<Field
-										type="date"
-										name="date"
-										className="p-0 form-control"
-									/>
-									</div>
-									<div className="d-flex flex-row justify-content-between">
-
-									<label htmlFor="date">Due Date</label>
-									<Field
-										type="text"
-										name="due_date"
-										className="p-1 form-control"
-									/>
-									</div>
-									<div className="d-flex flex-row justify-content-between">
-
-										<label htmlFor="terms">Terms</label>
 										<Field
-											type="type"
-											name="terms"
-											className="p-1 form-control"
+											as="textarea"
+											placeholder="Receiver's information"
+											className="form-control"
+											name="receiver"
 										/>
+									</Col>
+									<Col className="d-flex flex-column justify-content-end">
+										<Field
+											as="textarea"
+											placeholder="Shipping details"
+											className="form-control"
+											name="shipping_address"
+										/>
+									</Col>
+									<Col className="d-flex flex-column justify-content-end">
+										<div className="d-flex flex-row justify-content-between">
+											<label htmlFor="bill_number">
+												Number
+											</label>
+											<Field
+												type="text"
+												name="bill_number"
+												className="p-0 form-control"
+											/>
 										</div>
 										<div className="d-flex flex-row justify-content-between">
-
-										<label htmlFor="terms">PO Box</label>
-										<Field
-											type="type"
-											name="po_number"
-											className="p-1 form-control"
-										/>
-									</div>
+											<label htmlFor="date">Date</label>
+											<Field
+												type="date"
+												name="on_date"
+												className="p-0 form-control"
+											/>
+										</div>
+										<div className="d-flex flex-row justify-content-between">
+											<label htmlFor="date">
+												Due Date
+											</label>
+											<Field
+												type="text"
+												name="due_date"
+												className="p-1 form-control"
+											/>
+										</div>
+										<div className="d-flex flex-row justify-content-between">
+											<label htmlFor="terms">Terms</label>
+											<Field
+												type="type"
+												name="terms"
+												className="p-1 form-control"
+											/>
+										</div>
+										<div className="d-flex flex-row justify-content-between">
+											<label htmlFor="terms">
+												PO Box
+											</label>
+											<Field
+												type="type"
+												name="po_number"
+												className="p-1 form-control"
+											/>
+										</div>
 									</Col>
-							</Row>
-							<Row className="bg-dark text-light rounded">
-								<Col md="7">Title</Col>
-								<Col md="1">Qty</Col>
-								<Col md="2">Rate</Col>
-								<Col md="2">Amount</Col>
-							</Row>
-							<FieldArray name="items">
-								{({ insert, remove, push }) => (
-									<>
-										{values.items.length > 0 &&
-											values.items.map((item, index) => (
-												<Row
-													key={index}
-													className="my-1">
-													<Col
-														md="7"
-														className="px-0">
-														<Field
-															name={`items.${index}.title`}
-															placeholder="Shoes"
-															type="text"
-															className="p-1 form-control"
-														/>
-														<ErrorMessage
-															name={`items.${index}.title`}
-															component="div"
-															className="field-error"
-														/>
-													</Col>
-													<Col
-														md="1"
-														className="px-0">
-														<Field
-															name={`items.${index}.qty`}
-															placeholder="ex:4"
-															type="text"
-															className="p-1 form-control"
-														/>
-														<ErrorMessage
-															name={`items.${index}.qty`}
-															component="div"
-															className="field-error"
-														/>
-													</Col>
-													<Col
-														md="2"
-														className="px-0">
-														<Field
-															name={`items.${index}.rate`}
-															placeholder="400"
-															type="text"
-															className="p-1 form-control"
-														/>
-														<ErrorMessage
-															name={`items.${index}.rate`}
-															component="div"
-															className="field-error"
-														/>
-													</Col>
-													<Col md="2" className="p-2">
-														{
-															(values.items[
-																index
-															].amount =
-																item.rate *
-																item.qty)
-														}
-														<span
-															className="secondary"
-															onClick={() =>
-																remove(index)
-															}>
-															X
-														</span>
-													</Col>
-												</Row>
-											))}
-										<Row>
-											<Col>
-												<span
-													className="secondary"
-													onClick={() =>
-														push({
-															product: "",
-															qty: 0,
-															rate: 0,
-															amount: 0,
-														})
-													}>
-													Add item
-												</span>
-											</Col>
-										</Row>
-									</>
+								</Row>
+								<Row className="bg-dark text-light rounded">
+									<Col md="7">Title</Col>
+									<Col md="1">Qty</Col>
+									<Col md="2">Rate</Col>
+									<Col md="2">Amount</Col>
+								</Row>
+								<FieldArray name="items">
+									{({ insert, remove, push }) => (
+										<>
+											{values.items.length > 0 &&
+												values.items.map(
+													(item, index) => (
+														<Row
+															key={index}
+															className="my-1">
+															<Col
+																md="7"
+																className="px-0">
+																<Field
+																	name={`items.${index}.title`}
+																	placeholder="Shoes"
+																	type="text"
+																	className="p-1 form-control"
+																/>
+																<ErrorMessage
+																	name={`items.${index}.title`}
+																	component="div"
+																	className="field-error"
+																/>
+															</Col>
+															<Col
+																md="1"
+																className="px-0">
+																<Field
+																	name={`items.${index}.qty`}
+																	placeholder="ex:4"
+																	type="text"
+																	className="p-1 form-control"
+																/>
+																<ErrorMessage
+																	name={`items.${index}.qty`}
+																	component="div"
+																	className="field-error"
+																/>
+															</Col>
+															<Col
+																md="2"
+																className="px-0">
+																<Field
+																	name={`items.${index}.rate`}
+																	placeholder="400"
+																	type="text"
+																	className="p-1 form-control"
+																/>
+																<ErrorMessage
+																	name={`items.${index}.rate`}
+																	component="div"
+																	className="field-error"
+																/>
+															</Col>
+															<Col
+																md="2"
+																className="p-2">
+																{
+																	(values.items[
+																		index
+																	].amount =
+																		item.rate *
+																		item.qty)
+																}
+																<span
+																	className="secondary"
+																	onClick={() =>
+																		remove(
+																			index
+																		)
+																	}>
+																	X
+																</span>
+															</Col>
+														</Row>
+													)
+												)}
+											<Row>
+												<Col className="p-0">
+													<span
+														className="secondary"
+														onClick={() =>
+															push({
+																title: "",
+																qty: "",
+																rate: "",
+																amount: "",
+															})
+														}>
+														Add item
+													</span>
+												</Col>
+											</Row>
+										</>
+									)}
+								</FieldArray>
+								<Row>
+									<Col>
+										<label htmlFor="notes">Notes</label>
+										<Field
+											name="notes"
+											id="notes"
+											placeholder="Notes"
+											as="textarea"
+											className="form-control"
+										/>
+										<label htmlFor="other_terms">
+											Terms
+										</label>
+										<Field
+											name="other_terms"
+											id="other_terms"
+											placeholder="Terms"
+											as="textarea"
+											className="form-control"
+										/>
+									</Col>
+									<Col>
+										<div>
+											Sub Total
+											{subTotal(values) || 0}
+
+										</div>
+										<label htmlFor="discount">
+											Discount
+										</label>
+										<Field
+											name="discount"
+											id="discount"
+											type="text"
+											className="form-control"
+										/>
+										<label htmlFor="tax">
+											Tax
+										</label>
+										<Field
+											name="tax"
+											id="tax"
+											type="text"
+											className="form-control"
+										/>
+										<label htmlFor="shipping">
+											Shipping
+										</label>
+										<Field
+											name="shipping"
+											id="shipping"
+											type="text"
+											className="form-control"
+										/>
+										<p>
+											Total
+										</p>
+										<label htmlFor="paid">
+											Paid
+										</label>
+										<Field
+											name="paid"
+											id="paid"
+											type="text"
+											className="form-control"
+										/>
+										<p>
+											Balance Due
+										</p>
+									</Col>
+								</Row>
+							</Col>
+							<Col md="3">
+								{/* <button type="submit">Download</button> */}
+								{ subTotal(values) > 0 && (
+									<PDFDownloadLink
+										document={<PDFDoc data={values} />}
+										fileName={
+											`#${values?.bill_number}.pdf` ||
+											"#invoice.pdf"
+										}>
+										{({ loading }) =>
+											loading
+												? "Loading document"
+												: "Download"
+										}
+									</PDFDownloadLink>
 								)}
-							</FieldArray>
-							<div>
-								Grand Total
-								{values.items.reduce(
-									(accumulator, item) =>
-										accumulator + item.amount,
-									0
-								)}
-							</div>
-						</Col>
-						<Col md="3">
-							{/* <button type="submit">Download</button> */}
-							{values.items.reduce((accumulator, item) => accumulator + item.amount,0) > 0 && <PDFDownloadLink document={<PDFDoc data={values}/>} fileName={`#${values?.bill_number}.pdf` || '#invoice.pdf'}>
-								{({loading}) => (loading ? "Loading document" : "Download")}
-							</PDFDownloadLink> }
-						</Col>
-					</Row>
-				</Form>
-			)}
-		</Formik>
-	</div>
-);
+							</Col>
+						</Row>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
+};
 
 export default Invoice;
