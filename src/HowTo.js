1f34c19dfd67412508c45f003ac4981e28fcd71f
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-06-12 13:06:54 +0300

    Customizing screens tailwind css. Converting to tailwind

--- src/HowTo.js
+++ src/HowTo.js
@@ -1,29 +1,38 @@
-
 function HowTo() {
   return (
-    <div className="py-3">
-      <p className="py-2">
-        This Free app will save your time. Determines how much each team member
-        will get from a shareable amount.
-      </p>
-      <p className="font-bold mt-2 mb-4 text-lg">
-        3 Simple steps
-      </p>
-      <div className="flex justify-between bg-[#F8F9FA]">
-        <div className="p-4">
-          <h4 className="text-2xl text-[#6B7280]">1. Initiate Pool</h4>
-          <p className="py-1">Add amount you want to share.</p>
-        </div>
-        <div className="p-4 col-md-4">
-          <h4 className="text-2xl text-[#6B7280]">2. Beneficiaries</h4>
-          <p className="py-1">
-            Enter the Name and Percentage of each person. Made a mistake? No
-            worries you can edit or delete.
-          </p>
-        </div>
-        <div className="p-4 col-md-4">
-          <h4 className="text-2xl text-[#6B7280]">3. Breakdown</h4>
-          <p className="py-1">See the breakdown of the amount. Click Reset to start a fresh.</p>
+    <div className="py-3 w-screen">
+      <div className="xs:mx-5 sm:mx-10 md:mx-20 rounded-sm">
+        <p className="py-2 mx-2">
+          This Free app will save your time. Determines how much each team
+          member will get from a shareable amount.
+        </p>
+        <p className="font-bold mt-2 mb-4 text-md mx-2">3 Simple steps</p>
+        <div className="grid md:grid-cols-3 sm:grid-cols-1 xs:gap-y-2">
+          <div className="px-6 py-6 bg-[#F8F9FA] md:rounded-none sm:rounded-md xs:rounded-sm mb-2">
+            <h4 className="text-xl text-[#6B7280] font-semibold">
+              1. Initiate Pool
+            </h4>
+            <p className="py-1 text-md text-[#212529]">
+              Add amount you want to share.
+            </p>
+          </div>
+          <div className="px-6 py-6 bg-[#F8F9FA] md:rounded-none sm:rounded-md xs:rounded-sm mb-2">
+            <h4 className="text-xl text-[#6B7280] font-semibold">
+              2. Beneficiaries
+            </h4>
+            <p className="py-1 text-md text-[#212529]">
+              Enter the Name and Percentage of each person. Made a mistake? No
+              worries you can edit or delete.
+            </p>
+          </div>
+          <div className="px-6 py-6 bg-[#F8F9FA] md:rounded-none sm:rounded-md xs:rounded-sm mb-2">
+            <h4 className="text-xl text-[#6B7280] font-semibold">
+              3. Breakdown
+            </h4>
+            <p className="py-1 text-md text-[#212529]">
+              See the breakdown of the amount. Click Reset to start a fresh.
+            </p>
+          </div>
         </div>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 // import "./custom.css";
 import "./index.css";
 import Routing from "./Routing";
