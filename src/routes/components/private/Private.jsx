dec0bb20c6e4a305a97eede0b0ab6b6bbad8997e
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-06-15 16:16:30 +0300

    Private route refactor

--- src/routes/components/private/Private.jsx
+++ src/routes/components/private/Private.jsx
@@ -1,19 +1,9 @@
 import { useAuth } from "../../../hooks";
 import { Navigate, useLocation } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { AppwriteService as appwrite } from "../../../services";
 
 function Private({ children }) {
   const { pathname: from } = useLocation();
-  const { session } = useAuth();
-  const [ user, setUser] = useState(null)
-
-  useEffect(() => {
-    appwrite.getAccount().then(
-      user => {
-      setUser(user)
-    });
-  }, [ session ]);
+  const { user } = useAuth();
   return user ? children : <Navigate to="/login" state={{ from }} />;
 }
 
