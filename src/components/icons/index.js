9dec78ed9fea26f67cf07e94135bea35044751fb
David <davidderrickanyuru@gmail.com>
2024-09-20 17:28:19 +0300

    feat: create icons

---
+++ src/components/icons/index.js
@@ -0,0 +1,7 @@
+import Facebook from "./Facebook";
+import Instagram from "./Instagram";
+import LinkedIn from "./LinkedIn"
+
+import Twitter from "./Twitter";
+
+export { Facebook, Instagram, LinkedIn, Twitter };
