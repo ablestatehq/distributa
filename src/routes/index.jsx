32ed401b5d19ac7ff542b7dc142a49b1898d0a04
David <davidderrickanyuru@gmail.com>
2024-09-20 15:23:26 +0300

    feat: add the not found layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,6 +2,17 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { publicRoutes } from "./public";
 import { protectedRoutes } from "./protected";
 import { NotFound } from "./components";
+import { PublicNav, Footer } from "../Layouts/components";
+
+function NotFoundLayout({ children }) {
+  return (
+    <main className="flex flex-col min-h-screen container mx-auto lg:max-w-6xl">
+      <PublicNav />
+      {children}
+      <Footer />
+    </main>
+  );
+}
 
 function Routes() {
   const router = createBrowserRouter([
@@ -9,7 +20,11 @@ function Routes() {
     ...protectedRoutes,
     {
       path: "*",
-      element: <NotFound />,
+      element: (
+        <NotFoundLayout>
+          <NotFound />
+        </NotFoundLayout>
+      ),
     },
   ]);
   return <RouterProvider router={router}></RouterProvider>;
