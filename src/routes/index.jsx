386255817b3a6c0429f64db847faba46f06500de
David <davidderrickanyuru@gmail.com>
2024-09-20 13:35:49 +0300

    chore: remove the main layout

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,11 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { publicRoutes } from "./public";
 import { protectedRoutes } from "./protected";
-import { Main } from "../Layouts";
 import { NotFound } from "./components";
 
 function Routes() {
   const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main />,
-      children: publicRoutes,
-    },
+    ...publicRoutes,
     ...protectedRoutes,
     {
       path: "*",
