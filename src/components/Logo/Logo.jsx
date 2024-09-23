07135a2c44de14d87c2858353346a8af81f1fb60
David <davidderrickanyuru@gmail.com>
2024-09-23 13:40:17 +0300

    chore: remove unnecessary attributes

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -5,7 +5,6 @@ const Logo = ({ className }) => {
   return (
     <svg
       viewBox="0 0 128 24"
-      fill="none"
       xmlns="http://www.w3.org/2000/svg"
       className={cn(
         className,
