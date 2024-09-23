abf3123f5d86ba22ab8462298c0cee951fc4fc4e
David <davidderrickanyuru@gmail.com>
2024-09-23 13:43:35 +0300

    create list icon

---
+++ src/components/icons/List.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+const List = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 65"
+      className="fill-none w-16 h-[4.0625rem]"
+    >
+      <rect x="0.5" y="1.38354" width="63" height="63" rx="3.5" fill="white" />
+      <rect
+        x="0.5"
+        y="1.38354"
+        width="63"
+        height="63"
+        rx="3.5"
+        stroke="#B8B8B8"
+      />
+      <path
+        d="M28.1665 26.925H41.1665"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M28.1665 32.925H41.1665"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M28.1665 38.925H41.1665"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M23.1665 26.925H23.1765"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M23.1665 32.925H23.1765"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M23.1665 38.925H23.1765"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default List;
