a0d3e3cb6d104929d9cfda1f3a5fb7cdeec0ffc3
David <davidderrickanyuru@gmail.com>
2024-09-23 13:44:06 +0300

    create user icon

---
+++ src/components/icons/UserPlus.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+
+const UserPlus = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 65"
+      className="fill-none w-16 h-[4.0625rem]"
+    >
+      <rect x="0.5" y="1.38354" width="63" height="63" rx="3.5" fill="white" />
+      <rect
+        x="0.5"
+        y="1.38354"
+        width="63"
+        height="63"
+        rx="3.5"
+        stroke="#B8B8B8"
+      />
+      <g clip-path="url(#clip0_525_279)">
+        <path
+          d="M35.8335 41.925V39.925C35.8335 38.8642 35.4121 37.8468 34.6619 37.0966C33.9118 36.3465 32.8944 35.925 31.8335 35.925H24.8335C23.7726 35.925 22.7552 36.3465 22.0051 37.0966C21.2549 37.8468 20.8335 38.8642 20.8335 39.925V41.925"
+          stroke="#0D6EFD"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M28.3335 31.925C30.5426 31.925 32.3335 30.1342 32.3335 27.925C32.3335 25.7159 30.5426 23.925 28.3335 23.925C26.1244 23.925 24.3335 25.7159 24.3335 27.925C24.3335 30.1342 26.1244 31.925 28.3335 31.925Z"
+          stroke="#0D6EFD"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M39.8335 28.925V34.925"
+          stroke="#0D6EFD"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+        <path
+          d="M42.8335 31.925H36.8335"
+          stroke="#0D6EFD"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_525_279">
+          <rect
+            width="24"
+            height="24"
+            fill="white"
+            transform="translate(19.8335 20.925)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default UserPlus;
