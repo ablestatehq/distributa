180ca16a72c4f2b9948e948a6c5f1ed4f8d32b7d
David <davidderrickanyuru@gmail.com>
2024-09-23 13:42:32 +0300

    create folder icon

---
+++ src/components/icons/Folder.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Folder = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 64 65"
+      className="fill-none w-16 h-[4.0625rem]"
+    >
+      <rect x="0.5" y="1.38348" width="63" height="63" rx="3.5" fill="white" />
+      <rect
+        x="0.5"
+        y="1.38348"
+        width="63"
+        height="63"
+        rx="3.5"
+        stroke="#B8B8B8"
+      />
+      <path
+        d="M41.5 39.925C41.5 40.4554 41.2893 40.9641 40.9142 41.3392C40.5391 41.7143 40.0304 41.925 39.5 41.925H23.5C22.9696 41.925 22.4609 41.7143 22.0858 41.3392C21.7107 40.9641 21.5 40.4554 21.5 39.925V25.925C21.5 25.3946 21.7107 24.8858 22.0858 24.5108C22.4609 24.1357 22.9696 23.925 23.5 23.925H28.5L30.5 26.925H39.5C40.0304 26.925 40.5391 27.1357 40.9142 27.5108C41.2893 27.8858 41.5 28.3946 41.5 28.925V39.925Z"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M31.5 31.925V37.925"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M28.5 34.925H34.5"
+        stroke="#0D6EFD"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Folder;
