71d2fb64e3b1dec63ffd7db700ae57c3165aefb5
David <davidderrickanyuru@gmail.com>
2024-09-24 18:31:24 +0300

    feat(validation): create beneficiary validation schema

--- src/features/authentication/utils/validator.js
+++ src/features/authentication/utils/validator.js
@@ -9,3 +9,11 @@ export const signUpSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Email is required"),
   password: Yup.string().min(8, "Too Short!").required("Password is required"),
 });
+
+export const beneficiarySchema = Yup.object().shape({
+  name: Yup.string().required("Name required"),
+  percentage: Yup.number()
+    .min(0, "Too low (Min: 0%)")
+    .max(100, "To high (Max: 100%)"),
+  amount: Yup.number().required("Amount is required"),
+});
