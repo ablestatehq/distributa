6eeff3622473e9a8b47f9eb6332f35d9e11915a7
David <davidderrickanyuru@gmail.com>
2024-10-01 10:42:55 +0300

    chore(Home): update the icons on the home page

---
+++ src/components/Home/IconWrapper.jsx
@@ -0,0 +1,9 @@
+function IconWrapper({ children }) {
+  return (
+    <div className="flex justify-center items-center bg-white border border-greyborder rounded w-16 h-16">
+      {children}
+    </div>
+  );
+}
+
+export default IconWrapper;
