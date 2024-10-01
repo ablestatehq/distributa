5c25ddc7ffb3b467dfe568df585d43b739b5e9c8
David <davidderrickanyuru@gmail.com>
2024-10-01 10:52:29 +0300

    feat(navigation): add navigation to the layout.

--- src/routes/components/privateRoute/PrivateRoute.jsx
+++ src/routes/components/privateRoute/PrivateRoute.jsx
@@ -1,16 +1,26 @@
 import { Outlet, useLoaderData } from "react-router-dom";
-import { PrivateNav } from "../../../Layouts/components";
+import { SideBar, Header, MobileNav } from "../../../Layouts/components";
 import { Await } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 
 function PrivateRoute() {
   const data = useLoaderData();
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const handleToggleMenu = () => setToggleMenu((toggleMenu) => !toggleMenu);
+
   return (
     <Suspense fallback={<div>loading...</div>}>
       <Await resolve={data}>
-        <PrivateNav />
-        <div className="w-screen">
-          <Outlet />
+        <div className="w-screen h-screen overflow-scroll flex flex-col md:flex-row justify-end">
+          <Header handleToggleMenu={handleToggleMenu} toggleMenu={toggleMenu} />
+          <SideBar />
+          <div className="relative md:static flex-1 h-full w-full md:flex md:flex-row border border-red-500">
+            <MobileNav
+              toggleMenu={toggleMenu}
+              handleMenuToggle={handleToggleMenu}
+            />
+            <Outlet />
+          </div>
         </div>
       </Await>
     </Suspense>
