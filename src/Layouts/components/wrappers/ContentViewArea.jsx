3984be0edf718f0834f6e6e668f8e21b828002ed
David <davidderrickanyuru@gmail.com>
2024-10-01 12:55:57 +0300

    chore: Add paddding for the large screens

--- src/Layouts/components/wrappers/ContentViewArea.jsx
+++ src/Layouts/components/wrappers/ContentViewArea.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentViewArea = ({ children }) => {
   return (
-    <section className="p-6 flex flex-col h-full w-full">{children}</section>
+    <section className="p-6 md:py-16 md:px-8 flex flex-col h-full w-full">{children}</section>
   );
 };
 
