524b1bdbdbabd0c399e4bd645b9a32601b5c8c2d
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-10-08 16:38:23 +0300

    chore: add scroll behaviour on overflow

--- src/Layouts/components/wrappers/ContentViewArea.jsx
+++ src/Layouts/components/wrappers/ContentViewArea.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ContentViewArea = ({ children }) => {
   return (
-    <section className="p-6 md:py-16 md:px-8 flex-1 flex flex-col w-full overflow-hidden">
+    <section className="p-6 md:py-16 md:px-8 flex-1 flex flex-col h-full w-full overflow-auto">
       {children}
     </section>
   );
