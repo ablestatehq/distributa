87dadbf30b07123257a9a51b331c4fa9749801ad
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-10-31 17:30:21 +0300

    feat(share invoice): initialize sharing invoice

--- src/features/invoicing/routes/index.jsx
+++ src/features/invoicing/routes/index.jsx
@@ -1,8 +1,13 @@
-import { Invoice } from "../components"
+import { Invoice } from "../components";
 
 export const invoicingRoutes = [
   {
     path: "invoice",
     element: <Invoice />,
+  },
+  {
+    path: "invoice/shared/:id",
+    element: <div>Show the Invoice</div>,
+    //TODO: implement the loader to execute
   },
 ];
