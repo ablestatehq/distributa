a7f032e253a74c27232f377b8b5ae5aba143cfe8
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-10-31 14:49:08 +0300

    chore: integrate services

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,13 @@
 import AppwriteService from "./appwrite";
 import InvoiceService from "./invoice.service";
 import TransactionService from "./transaction.service";
+import BalancesService from "./balances.service";
+import CategoryService from "./categories.service";
 
-export { AppwriteService, InvoiceService, TransactionService };
+export {
+  AppwriteService,
+  InvoiceService,
+  TransactionService,
+  BalancesService,
+  CategoryService,
+};
