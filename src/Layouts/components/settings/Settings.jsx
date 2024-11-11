d4f7cb0fbc39de06781cc08bdf90a19981021c66
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-11-11 13:28:02 +0300

    feat(settings): underline navigation on current tab

--- src/Layouts/components/settings/Settings.jsx
+++ src/Layouts/components/settings/Settings.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { ContentViewAreaWrapper } from "..";
+import cn from "../../../utils/cn";
 
 const Settings = () => {
   return (
@@ -10,9 +11,34 @@ const Settings = () => {
           Settings
         </h1>
         <nav className="flex gap-x-2">
-          <NavLink to="/settings/profile">Profile</NavLink>
-          <NavLink to="/settings/billing">Billing</NavLink>
-          <NavLink to="/settings/categories">Categories</NavLink>
+          <NavLink
+            to="/settings/profile"
+            className={({ isActive }) =>
+              cn(
+                "px-0.5 pb-1 pt-3 font-satoshi font-normal leading-100 tracking-normal ease-in-out delay-75 transition-all",
+                {
+                  "border-b border-b-black": isActive,
+                  "border-b border-b-transparent": !isActive,
+                }
+              )
+            }
+          >
+            Profile
+          </NavLink>
+          <NavLink
+            to="/settings/categories"
+            className={({ isActive }) =>
+              cn(
+                "px-0.5 pb-1 pt-3 font-satoshi font-normal leading-100 tracking-normal ease-in-out delay-75 transition-all",
+                {
+                  "border-b border-b-black": isActive,
+                  "border-b border-b-transparent": !isActive,
+                }
+              )
+            }
+          >
+            Categories
+          </NavLink>
         </nav>
       </header>
       <main className="flex-1">
