fa86808a5341fb2091e7e0be2f487c113bde9179
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-11-25 16:08:09 +0300

    feat(invoices): create download icon

---
+++ src/components/common/icons/Download.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Download = () => {
+  return (
+    //     <svg
+    //     xmlns="http://www.w3.org/2000/svg"
+    //     className="fill-none h-3 w-3"
+    //     viewBox="0 0 12 12"
+    //   >
+    //     <path
+    //       stroke="#111"
+    //       strokeLinecap="round"
+    //       strokeLinejoin="round"
+    //       className="stroke-[#111] group-hover:stroke-accent"
+    //       d="M10.5 7.5v2a1 1 0 0 1-1 1h-7a1 1 0 0 1-1-1v-2m2-2.5L6 7.5m0 0L8.5 5M6 7.5v-6"
+    //     ></path>
+    //   </svg>
+    <svg
+      className="fill-none h-3 w-3"
+      viewBox="0 0 12 12"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_3760_1116)">
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M1 8.647v1.177c0 .312.132.61.366.831.235.221.552.345.884.345h7.5c.332 0 .65-.124.884-.345s.366-.52.366-.831V8.647m-8.125-3.53L6 8.06m0 0 3.125-2.941M6 8.058V1"
+          className="stroke-[#111] group-hover:stroke-accent"
+        ></path>
+      </g>
+      <defs>
+        <clipPath id="clip0_3760_1116">
+          <path fill="#fff" d="M0 0h12v12H0z"></path>
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default Download;
