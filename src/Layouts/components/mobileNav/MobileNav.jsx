04cd8d42e2cb485e902dee9db9fa2abe73ba3e93
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-11-29 16:08:31 +0300

    chore: add overlay on show of mobile nav

--- src/Layouts/components/mobileNav/MobileNav.jsx
+++ src/Layouts/components/mobileNav/MobileNav.jsx
@@ -9,60 +9,68 @@ const MobileNav = ({ toggleMenu, handleMenuToggle }) => {
   const navigate = useNavigate();
 
   return (
-    <aside
-      className={`top-0 right-0 absolute h-full md:hidden flex-col justify-between bg-white items-end py-16 px-8 z-20 ${cn(
+    <div
+      className={`top-0 right-0 absolute h-full w-full bg-black bg-opacity-45 flex justify-end ${cn(
         {
-          flex: toggleMenu,
           hidden: !toggleMenu,
         }
       )}`}
     >
-      <nav className="flex flex-col gap-y-4">
-        <NavigationLink
-          to="/invoices"
-          Icon={FileText}
-          children="My invoices"
-          exact={true}
-          onClick={handleMenuToggle}
-        />
-        <NavigationLink
-          to="/invoices/new"
-          Icon={PlusSquare}
-          children="New Invoice"
-          exact={true}
-          onClick={handleMenuToggle}
-        />
-        <NavigationLink
-          to="/transactions"
-          Icon={Book}
-          children="My Transactions"
-          exact={true}
-          onClick={handleMenuToggle}
-        />
-      </nav>
+      <aside
+        className={`h-full w-fit md:hidden flex-col justify-between bg-white items-end py-16 px-8 z-20 ${cn(
+          {
+            flex: toggleMenu,
+            hidden: !toggleMenu,
+          }
+        )}`}
+      >
+        <nav className="flex flex-col gap-y-4">
+          <NavigationLink
+            to="/invoices"
+            Icon={FileText}
+            children="My invoices"
+            exact={true}
+            onClick={handleMenuToggle}
+          />
+          <NavigationLink
+            to="/invoices/new"
+            Icon={PlusSquare}
+            children="New Invoice"
+            exact={true}
+            onClick={handleMenuToggle}
+          />
+          <NavigationLink
+            to="/transactions"
+            Icon={Book}
+            children="My Transactions"
+            exact={true}
+            onClick={handleMenuToggle}
+          />
+        </nav>
 
-      <div className="flex flex-col gap-y-4">
-        <button
-          className="text-black w-fit px-8"
-          onClick={async () => {
-            await logout();
-            handleMenuToggle();
-            navigate("/");
-          }}
-        >
-          Logout
-        </button>
-        <button
-          className="text-black w-fit px-8"
-          onClick={() => {
-            handleMenuToggle();
-            navigate("/settings/profile");
-          }}
-        >
-          Settings
-        </button>
-      </div>
-    </aside>
+        <div className="flex flex-col gap-y-4">
+          <button
+            className="text-black w-fit px-8"
+            onClick={async () => {
+              await logout();
+              handleMenuToggle();
+              navigate("/");
+            }}
+          >
+            Logout
+          </button>
+          <button
+            className="text-black w-fit px-8"
+            onClick={() => {
+              handleMenuToggle();
+              navigate("/settings/profile");
+            }}
+          >
+            Settings
+          </button>
+        </div>
+      </aside>
+    </div>
   );
 };
 
