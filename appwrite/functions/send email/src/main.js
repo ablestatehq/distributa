93ea132cfc7b84157883b44e97252f9b8c8a79ca
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2024-12-05 19:10:07 +0300

    wip: message on signup

---
+++ appwrite/functions/send email/src/main.js
@@ -0,0 +1,54 @@
+import { Client, Messaging, ID } from "node-appwrite";
+
+export default async ({ req, res, error, log }) => {
+  const client = new Client()
+    .setEndpoint(process.env.PROJECT_ENDPOINT)
+    .setProject(process.env.PROJECT_ID)
+    .setKey(process.env.API_KEY);
+
+  const messaging = new Messaging(client);
+
+  log(req.body);
+
+  const {
+    subject,
+    content,
+    topics = [],
+    users = [],
+    targets = [],
+    cc = [],
+    bcc = [],
+    attachments = [],
+    draft = false,
+    html = false,
+    scheduled_at = "",
+  } = JSON.parse(req.body);
+
+  try {
+    const response = messaging.createEmail(
+      ID.unique(),
+      subject,
+      content,
+      topics,
+      users,
+      targets,
+      cc,
+      bcc,
+      attachments,
+      draft,
+      html,
+      scheduled_at
+    );
+
+    log(response);
+
+    if (response) return res.json({ success: true, data: response });
+  } catch (err) {
+    error(JSON.stringify(err, null, 2));
+
+    return res.json(
+      { success: false, error: JSON.stringify(error, null, 2) },
+      { status: 400 }
+    );
+  }
+};
