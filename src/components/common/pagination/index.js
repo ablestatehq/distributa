1fe95d1e0479c8ca34e8f1f57e50f7e22c304d07
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2025-04-09 12:39:36 +0300

    chore: pagination accessibility

---
+++ src/components/common/pagination/index.js
@@ -0,0 +1,6 @@
+export { Pagination } from "./pagination";
+export { PaginationButton } from "./components/PaginationButton";
+export { PageNumber } from "./components/PageNumber";
+export { PageSizeSelector } from "./PageSizeSelector";
+
+export * from "./utils/paginationUtils";
