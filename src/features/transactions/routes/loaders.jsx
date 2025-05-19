7eb85fb227c3c81ae3cfd948b6d6cbe3f74ba3f8
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2025-05-19 18:38:17 +0300

    feat: setup transaction routes

---
+++ src/features/transactions/routes/loaders.jsx
@@ -0,0 +1,7 @@
+import { account } from "../../../lib/appwrite/client";
+
+export async function transactionsLoader() {}
+
+export async function transactionLoader() {}
+
+export async function editTransactionLoader() {}
---
+++ src/features/transactions/routes/protected.jsx
@@ -0,0 +1,17 @@
+export default [
+  {
+    path: "/transactions",
+  },
+  {
+    path: "/transactions/new",
+  },
+  {
+    path: "/transactions/:id",
+  },
+  {
+    path: "/transactions/:id/edit",
+  },
+  {
+    path: "/transactions/:id/delete",
+  },
+];
